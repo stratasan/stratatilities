10a534681e84a89dce22d95f737607ad23b226a2
Scott Burns <scott.s.burns@gmail.com>
2019-01-03 07:57:55 -0600

    Black :all_the_things:

--- stratatilities/auth.py
+++ stratatilities/auth.py
@@ -3,6 +3,7 @@ import getpass
 import json
 import logging
 import os
+
 try:
     from urllib.parse import urlparse
 except ImportError:
@@ -11,8 +12,10 @@ except ImportError:
 import boto3
 import hvac
 import requests
+
 try:
     from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 except ImportError:
     pass
@@ -24,17 +27,15 @@ def headers_to_go_style(headers):
     retval = {}
     for k, v in headers.items():
         if isinstance(v, bytes):
-            retval[k] = [str(v, 'ascii')]
+            retval[k] = [str(v, "ascii")]
         else:
             retval[k] = [v]
     return retval
 
 
-def get_token(vault_addr,
-              iam_role_name,
-              iam_request_body,
-              iam_request_url,
-              iam_request_headers):
+def get_token(
+    vault_addr, iam_role_name, iam_request_body, iam_request_url, iam_request_headers
+):
     payload = {
         "role": iam_role_name,
         "iam_http_request_method": "POST",
@@ -42,62 +43,62 @@ def get_token(vault_addr,
         "iam_request_body": iam_request_body,
         "iam_request_headers": iam_request_headers,
     }
-    headers = {'content-type': 'application/json'}
+    headers = {"content-type": "application/json"}
     response = requests.post(
-        '{}/v1/auth/aws/login'.format(vault_addr),
+        "{}/v1/auth/aws/login".format(vault_addr),
         verify=False,
         timeout=5,
         data=json.dumps(payload),
-        headers=headers
+        headers=headers,
     )
-    return response.json()['auth']['client_token']
+    return response.json()["auth"]["client_token"]
 
 
 def request_vault_token(vault_addr):
     """ Requests a Vault Token from an IAM Role """
     session = boto3.session.Session()
-    client = session.client('sts')
+    client = session.client("sts")
     endpoint = client._endpoint
-    role_name = client.get_caller_identity()['Arn'].split('/')[1]
+    role_name = client.get_caller_identity()["Arn"].split("/")[1]
     logger.debug('Requesting vault token with IAM role name "%s"', role_name)
 
-    operation_model = client._service_model.operation_model('GetCallerIdentity')
+    operation_model = client._service_model.operation_model("GetCallerIdentity")
     request_dict = client._convert_to_request_dict({}, operation_model)
 
     awsIamServerId = urlparse(vault_addr).hostname
-    request_dict['headers']['X-Vault-awsiam-Server-Id'] = awsIamServerId
+    request_dict["headers"]["X-Vault-awsiam-Server-Id"] = awsIamServerId
 
-    logger.debug('Creating request with request_dict "%s" and operation_model "%s"',
-        request_dict, operation_model)
+    logger.debug(
+        'Creating request with request_dict "%s" and operation_model "%s"',
+        request_dict,
+        operation_model,
+    )
     request = endpoint.create_request(request_dict, operation_model)
     return get_token(
         vault_addr,
         role_name,
-        str(base64.b64encode(request.body.encode('ascii')), 'ascii'),
-        str(base64.b64encode(request.url.encode('ascii')), 'ascii'),
+        str(base64.b64encode(request.body.encode("ascii")), "ascii"),
+        str(base64.b64encode(request.url.encode("ascii")), "ascii"),
         str(
             base64.b64encode(
-                bytes(
-                    json.dumps(headers_to_go_style(dict(request.headers))),
-                    'ascii',
-                ),
+                bytes(json.dumps(headers_to_go_style(dict(request.headers))), "ascii")
             ),
-            'ascii',
-        )
+            "ascii",
+        ),
     )
 
 
-def get_vault_client(vault_addr=os.environ.get('VAULT_ADDR')):
-    vault_token = os.environ.get('VAULT_TOKEN', None)
+def get_vault_client(vault_addr=os.environ.get("VAULT_ADDR")):
+    vault_token = os.environ.get("VAULT_TOKEN", None)
     if not vault_token:
         vault_token = request_vault_token(vault_addr)
-    logger.debug('vault token: %s', vault_token)
+    logger.debug("vault token: %s", vault_token)
     return hvac.Client(url=vault_addr, verify=False, token=vault_token)
 
 
 def get_vault_client_via_ldap(
-        username, mount_point='ldap',
-        vault_addr=os.environ.get('VAULT_ADDR')):
+    username, mount_point="ldap", vault_addr=os.environ.get("VAULT_ADDR")
+):
     """ Return an authenticated vault client via LDAP.
 
     Password will be acquired via `getpass.getpass`. Services should
@@ -109,22 +110,22 @@ def get_vault_client_via_ldap(
     # with an incorrect password, an InvalidRequest is raised
     client.auth.ldap.login(
         username=username,
-        password=getpass.getpass('LDAP Password:'),
-        mount_point=mount_point
+        password=getpass.getpass("LDAP Password:"),
+        mount_point=mount_point,
     )
-    assert client.is_authenticated(), 'Client is not authenticated!'
+    assert client.is_authenticated(), "Client is not authenticated!"
     return client
 
 
-def return_token(vault_addr=os.environ.get('VAULT_ADDR')):
-    vault_token = os.environ.get('VAULT_TOKEN', None)
+def return_token(vault_addr=os.environ.get("VAULT_ADDR")):
+    vault_token = os.environ.get("VAULT_TOKEN", None)
     if not vault_token:
         vault_token = request_vault_token(vault_addr)
-    logger.debug('vault token: %s', vault_token)
+    logger.debug("vault token: %s", vault_token)
     return vault_token
 
 
-def read_vault_secret(vault_client, path_to_secret, vault_value_key='value'):
+def read_vault_secret(vault_client, path_to_secret, vault_value_key="value"):
     """ Read a vault secret given a {vault_client} and {path_to_secret}
 
     If the secret is "complex", in that it's more than just key/value,
@@ -135,9 +136,9 @@ def read_vault_secret(vault_client, path_to_secret, vault_value_key='value'):
     vault_value = None
     try:
         if vault_value_key is None:
-            vault_value = vault_client.read(path_to_secret)['data']
+            vault_value = vault_client.read(path_to_secret)["data"]
         else:
-            vault_value = vault_client.read(path_to_secret)['data'][vault_value_key]
+            vault_value = vault_client.read(path_to_secret)["data"][vault_value_key]
     except TypeError:
         pass
     return vault_value
--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -4,27 +4,25 @@ from stratatilities.credentials import get_redshift_dsn
 
 
 def test_get_redshift_dsn():
-    path = 'stratatilities.credentials.read_vault_secret'
+    path = "stratatilities.credentials.read_vault_secret"
     with patch(path) as read_vault_secret:
         vault_client = Mock()
         read_vault_secret.side_effect = [
-            'host',
-            'port',
-            'dbname',
-            {'username': 'USERNAME', 'password': 'password'},
+            "host",
+            "port",
+            "dbname",
+            {"username": "USERNAME", "password": "password"},
         ]
 
         dsn = get_redshift_dsn(vault_client)
         # any capped chars in username must be lowered because redshift
-        assert dsn == 'postgresql://username:password@host:port/dbname?keepalives=1'  # noqa
+        assert (
+            dsn == "postgresql://username:password@host:port/dbname?keepalives=1"
+        )  # noqa
         calls = [
-            call(vault_client, 'secret/staging/shared/DB_REDSHIFT_HOST'),
-            call(vault_client, 'secret/staging/shared/DB_REDSHIFT_PORT'),
-            call(vault_client, 'secret/staging/shared/DB_REDSHIFT_NAME'),
-            call(
-                vault_client,
-                'database/creds/redshift-staging',
-                vault_value_key=None,
-            ),
+            call(vault_client, "secret/staging/shared/DB_REDSHIFT_HOST"),
+            call(vault_client, "secret/staging/shared/DB_REDSHIFT_PORT"),
+            call(vault_client, "secret/staging/shared/DB_REDSHIFT_NAME"),
+            call(vault_client, "database/creds/redshift-staging", vault_value_key=None),
         ]
         read_vault_secret.assert_has_calls(calls)
