8f84d7fc7dcddb9030f3b6490aab3f544ab50bc7
drs1832 <dave@stratasan.com>
2020-07-20 09:17:52 -0500

    adding comparable RDS data-source connection

--- stratatilities/credentials.py
+++ stratatilities/credentials.py
@@ -14,3 +14,18 @@ def get_redshift_dsn(vault_client):
     return template.format(
         username=username, password=password, host=host, port=port, dbname=dbname
     )
+
+
+def get_rds_dsn(vault_client):
+    host = read_vault_secret(vault_client, "secret/staging/shared/DB_DEFAULT_HOST",)
+    port = read_vault_secret(vault_client, "secret/staging/shared/DB_DEFAULT_PORT",)
+    dbname = read_vault_secret(vault_client, "secret/staging/shared/DB_DEFAULT_NAME",)
+    db_creds = read_vault_secret(
+        vault_client, "database/creds/rds-staging", vault_value_key=None,
+    )
+    username = db_creds["username"]
+    password = db_creds["password"]
+    template = "postgresql://{username}:{password}@{host}:{port}/{dbname}?keepalives=1"
+    return template.format(
+        username=username, password=password, host=host, port=port, dbname=dbname
+    )
